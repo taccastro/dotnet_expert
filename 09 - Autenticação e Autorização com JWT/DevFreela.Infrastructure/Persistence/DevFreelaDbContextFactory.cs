ecf7c58f5e167a14c2db303b90e8a54598821da0
Tiago Castro <31635449+taccastro@users.noreply.github.com>
2025-09-15 18:27:34 -0300

    add mod9 jwt + factory + cleanlauchsettings.json

---
+++ 09 - Autenticação e Autorização com JWT/DevFreela.Infrastructure/Persistence/DevFreelaDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContextFactory : IDesignTimeDbContextFactory<DevFreelaDbContext>
+    {
+        public DevFreelaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DevFreelaDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DevFreela;Trusted_Connection=True;");
+            return new DevFreelaDbContext(optionsBuilder.Options);
+        }
+    }
+}
