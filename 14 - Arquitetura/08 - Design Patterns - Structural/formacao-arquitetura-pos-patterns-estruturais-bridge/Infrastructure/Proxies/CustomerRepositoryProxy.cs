04effd34d5662e0e11324179950c53a003d5eb0b
Tiago Castro <31635449+taccastro@users.noreply.github.com>
2025-10-16 16:50:30 -0300

    pattern bridge

---
+++ 14 - Arquitetura/08 - Design Patterns - Structural/formacao-arquitetura-pos-patterns-estruturais-bridge/Infrastructure/Proxies/CustomerRepositoryProxy.cs
@@ -0,0 +1,36 @@
+﻿using AwesomeShopPatterns.API.Core.Entities;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AwesomeShopPatterns.API.Infrastructure.Proxies
+{
+    public class CustomerRepositoryProxy : ICustomerRepository
+    {
+        private readonly ICustomerRepository _repository;
+        private readonly IMemoryCache _cache;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CustomerRepositoryProxy(ICustomerRepository repository, IMemoryCache cache, IHttpContextAccessor httpContextAccessor)
+        {
+            _repository = repository;
+            _cache = cache;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public List<Customer> GetBlockedCustomers()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            if (httpContext == null)
+                return null;
+
+            if (httpContext.Request.Headers["x-role"] != "admin")
+                return null;
+
+            List<Customer> blockedCustomers = _cache.GetOrCreate("blocked-customers", c =>
+            {
+                return _repository.GetBlockedCustomers();
+            });
+
+            return blockedCustomers;
+        }
+    }
+}
